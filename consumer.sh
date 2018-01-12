47d56963abdcd3a318ff71320095c45198884e36
Francesco D'Aloisio <fdaloisio@gmail.com>
2018-01-13 00:21:56 +0100

    LPOP to RPOP

--- consumer.sh
+++ consumer.sh
@@ -3,12 +3,10 @@ export PORT=6379
 export HOST=127.0.0.1
 while :
 do
-	R=$(echo "LPOP work" | ./mimiredis.sh)
-	#F="./fs/$R.txt"
+	R=$(echo "RPOP work" | ./mimiredis.sh)
 	if [[ "$R" != "(nil)"  ]];then
 		echo "Creating $R"
 		d=$(date --iso-8601=seconds | sed 's/:/-/')
 		echo $d >> $R 
 	fi
-	#sleep 1
 done
--- producer.sh
+++ producer.sh
@@ -5,9 +5,8 @@ export HOST=127.0.0.1
 echo "Wake"
 while :
 do
-s=$(( $RANDOM % 5 ))
-sleep $s
-d=$(date --iso-8601=seconds | sed 's/[:+]/-/g')
-UUID=$(cat /proc/sys/kernel/random/uuid)
-echo "LPUSH work './fs/$UUID.date'" | ./mimiredis.sh
+	sleep $(( $RANDOM % 5 ))
+	UUID=$(cat /proc/sys/kernel/random/uuid)
+	echo "PUSHING ./fs/$UUID.date"
+	echo "LPUSH work './fs/$UUID.date'" | ./mimiredis.sh
 done
